5403510039dce4ff82c6951b49021c1004650384
Inseong <ispaik0602@gmail.com>
2026-03-20 02:16:31 +0900

    RobotRunner bug fixed

--- robot/include/robot/RobotRunner.h
+++ robot/include/robot/RobotRunner.h
@@ -3,18 +3,27 @@
 
 #include "RobotController.h"
 #include "RobotModel.h"
+#include "Controllers/LegController.h"
 
 class RobotRunner {
 public:
-    RobotRunner(RobotController*);
+    explicit RobotRunner(RobotController* robot_ctrl)
+        : _robotController(robot_ctrl), _model(&_params) {}
 
     void init();
+    void setupStep();
     void run();
-    void cleanup();
+    void finalizeStep();
 
-    virtual ~RobotRunner();
+    virtual ~RobotRunner() = default;
+
+    RobotController* _robotController;
+    RobotType _robotType;
+    RobotParams<double> _params;
 
 private:
+    RobotModel<double> _model;
+    LegController<double>* _legController;
     u64 _iterations = 0;
 };
 
--- robot/src/RobotRunner.cpp
+++ robot/src/RobotRunner.cpp
@@ -1,11 +1,18 @@
 #include "RobotRunner.h"
 
-RobotRunner::RobotRunner(RobotController*) {}
+void RobotRunner::init() {
+    
+}
 
-void RobotRunner::init() {}
+void RobotRunner::run() {
 
-void RobotRunner::run() {}
+}
 
-void RobotRunner::cleanup() {}
 
-RobotRunner::~RobotRunner() = default;
+void RobotRunner::setupStep() {
+
+}
+
+void RobotRunner::finalizeStep() {
+
+}
